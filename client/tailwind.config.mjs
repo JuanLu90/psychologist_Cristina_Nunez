109e4db42eed7af12cea54e36a29c607b9050f0c
Juan Luis <juanlu.calderon@gmail.com>
2024-06-03 18:15:19 +0200

    logo updated

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,3 +1,4 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,7 +7,23 @@ export default {
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
       },
+      textShadow: {
+        sm: "0 1px 2px rgba(0,0,0,0.3)",
+        DEFAULT: "0 2px 4px rgba(0,0,0,0.3)",
+        lg: "0 8px 16px rgba(0,0,0,0.3)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
