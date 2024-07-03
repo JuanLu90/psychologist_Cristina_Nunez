aabf466e7cc9d61c4ba62ddb402d6b0e62055a07
Juan Luis <juanlu.calderon@gmail.com>
2024-07-04 00:29:49 +0200

    como empezar icons updated

--- client/src/util.js
+++ client/src/util.js
@@ -23,13 +23,3 @@ export const headerCategories = [
   { redirectTo: "/supervisiones", name: "Supervisiones" },
   { redirectTo: "/contacto", name: "Contacto" },
 ];
-
-// "#d2a363"
-
-// Color Principal: #d8e3cd (un tono suave de verde)
-// Color Secundario: #a5c4a5 (un verde más oscuro, para contraste)
-// Color de Énfasis: #7a9f7a (un verde aún más oscuro, para resaltar elementos importantes)
-// Color Complementario: #cddee3 (un azul muy claro, que complementa el verde principal)
-// Color de Fondo: #f7f9f7 (un blanco casi puro, para un fondo limpio)
-// Color de Texto: #4d4d4d (un gris oscuro, para textos principales)
-// Color de Enlace: #6094a5 (un azul medio, para enlaces y llamadas a la acción)
